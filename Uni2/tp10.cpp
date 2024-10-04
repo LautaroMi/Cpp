0bda2f0dfe565960deb733abb204f981237e1d19
Lautaro <lautymiguenz@gmail.com>
2024-10-04 11:43:05 -0300

    Avance en tp10

---
+++ Uni2/tp10.cpp
@@ -0,0 +1,19 @@
+/*
+Ejercicio 10.
+La universidad posee información histórica sobre los estudiantes (no más de 1.000) de una materia determinada.
+De cada uno almacena: nombre y apellido, legajo, cantidad de inasistencias a clase, calificación obtenida.
+Almacenar esta información en un arreglo de structs. Se solicita generar los siguientes reportes:
+a) Imprimir nombre y apellido de los alumnos que tuvieron más de 5 inasistencias.
+b) Imprimir número de legajo de los alumnos cuya calificación promedio sea mayor o igual a la calificación
+promedio total (requiere calcular un promedio de las calificaciones de todos los alumnos en el listado).
+c) Imprimir el número de legajo de aquellos alumnos que tienen promedio mayor o igual a 9.
+d) Dado el legajo de un alumno, eliminarlo del listado.
+Nota: analizar si es posible reutilizar código en más de uno de los incisos.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+}
