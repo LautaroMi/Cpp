740a13601a4eef71ae8ad565439ff7bb1652ecaf
Lautaro <lautymiguenz@gmail.com>
2024-10-04 11:45:13 -0300

    Avances

--- Uni2/tp10.cpp
+++ Uni2/tp10.cpp
@@ -1,19 +1,87 @@
 /*
 Ejercicio 10.
 La universidad posee información histórica sobre los estudiantes (no más de 1.000) de una materia determinada.
-De cada uno almacena: nombre y apellido, legajo, cantidad de inasistencias a clase, calificación obtenida.
-Almacenar esta información en un arreglo de structs. Se solicita generar los siguientes reportes:
+De cada uno almacena: 
+nombre.
+apellido. 
+legajo. 
+cantidad de inasistencias a clase. 
+calificación obtenida.
+Almacenar esta información en un arreglo de structs. 
+Se solicita generar los siguientes reportes:
+
 a) Imprimir nombre y apellido de los alumnos que tuvieron más de 5 inasistencias.
+
 b) Imprimir número de legajo de los alumnos cuya calificación promedio sea mayor o igual a la calificación
 promedio total (requiere calcular un promedio de las calificaciones de todos los alumnos en el listado).
+
 c) Imprimir el número de legajo de aquellos alumnos que tienen promedio mayor o igual a 9.
+
 d) Dado el legajo de un alumno, eliminarlo del listado.
 Nota: analizar si es posible reutilizar código en más de uno de los incisos.
 */
 
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
+struct Estudiantes{
+    string nombre="";
+    string apellido="";
+    int legajo=0;
+    int inasistencias=0;
+    int nota=0;
+};
+
+int cargarEstudiante(Estudiantes arr[], int dl,const int DF){
+    Estudiantes alumno1;
+    while (alumno1.nombre != "Fin" && dl < DF){
+        cout <<"Nombre del alumno('Fin' para terminar): ";
+        cin >> alumno1.nombre;
+        if(alumno1.nombre == "Fin"){
+            continue;
+        }
+        cout <<"Apellido: ";
+        cin >> alumno1.apellido;
+        cout <<"Legajo: ";
+        cin >> alumno1.legajo;
+        cout <<"Inasistencias: ";
+        cin >> alumno1.inasistencias;
+        cout <<"Nota: ";
+        cin >> alumno1.nota;
+        arr[dl] = alumno1;
+
+        dl++; 
+    }
+    return dl;
+};
+
+void mostrarEstudiante(Estudiantes arr[],int dl){
+    for (int i = 0; i < dl; i++){
+        cout <<"Alumno " << i+1 << endl;
+        cout <<"Nombre: " << arr[i].nombre<< endl;
+        cout <<"Apellido: " << arr[i].apellido<< endl;
+        cout <<"Legajo: " << arr[i].legajo<< endl;
+        cout <<"Inasistencias: " << arr[i].inasistencias<< endl;
+        cout <<"Nota: " << arr[i].nota<< endl;
+    }
+    
+}
+
+
+
 int main(){
+
+    const int DF = 1000;
+    int dl = 0;
+
+    Estudiantes arr[DF];
+
+    dl = cargarEstudiante(arr,dl,DF);
+
+    mostrarEstudiante(arr,dl);
+
+
+
 
 }
